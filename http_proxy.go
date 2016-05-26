df43a1c59be7d459ab8a08e256c5a8d8d8e4e901
ssoor <swnetwork@yeah.net>
2016-05-26 17:05:30 +0800

    新增对直接重定向方式代理的支持

--- http_proxy.go
+++ http_proxy.go
@@ -82,6 +82,7 @@ func (h *HTTPProxy) ServeHTTPTunnel(response http.ResponseWriter, request *http.
 
 // ServeHTTP implements HTTP Handler
 func (h *HTTPProxy) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	request.URL, _ = url.ParseRequestURI("http://" + request.Host + request.RequestURI)
 	if request.Method == "CONNECT" {
 		h.ServeHTTPTunnel(response, request)
 	} else {
